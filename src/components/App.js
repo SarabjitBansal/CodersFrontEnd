6f8f4790b7f7d73f09138ee81e8e4d96d6d5cf4e
sonia08 <soniabansal@Sonias-MacBook-Pro.local>
2018-05-22 09:05:09 +1000

    chnages

--- src/components/App.js
+++ src/components/App.js
@@ -108,41 +108,202 @@ import _ from "lodash";
 import RaisedButton from "material-ui/RaisedButton";
 
 
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       currentChatMessage: "",
+//       chatLogs: [],
+//       user: {},
+//       loading: false
+//     };
+//   }
+//   componentWillMount() {
+//     debugger;
+//     const user = jwtDecoder(localStorage.getItem('jwtToken'));
+//     // const user = await jwtDecoder(this.props.token);
+//     console.log('CHAT USER',user);
+//      this.setState({
+//       user:user,
+//       loading: true
+//     });
+//     console.log(this.state.user);
+//     this.fetchHistoryMessages();
+//     this.createSocket();
+//
+//   }
+//   componentDidMount = async () => {
+//     debugger
+//     const user = jwtDecoder(localStorage.getItem('jwtToken'));
+//     // const user = await jwtDecoder(this.props.token);
+//     console.log('CHAT USER',user);
+//     await this.setState({
+//       user:user,
+//       loading: true
+//     });
+//
+//      this.fetchHistoryMessages();
+//
+//     await this.createSocket();
+//
+//     this.setState({
+//       loading: false
+//     });
+//   };
+//
+//   fetchHistoryMessages = () => {
+//     axios(
+//       {
+//         url: 'https://codersappserver.herokuapp.com/messages.json'
+//
+//       }
+//     )
+//       .then(res => {
+//         // console.log(chatLogs);
+//         this.setState({
+//           chatLogs: res.data
+//         })
+//       })
+//   }
+//
+//   createSocket = () => {
+//     // debugger;
+//     const user_name = this.state.user.name;
+//     const user_id = this.state.user.sub;
+//     console.log(user_id, user_name);
+//     let cable = Cable.createConsumer("wss://codersappserver.herokuapp.com/cable");
+//     this.chats = cable.subscriptions.create(
+//       {
+//         channel: `MessagesChannel`
+//       },
+//       {
+//         connected: () => {},
+//         received: data => {
+//           let chatLogs = this.state.chatLogs;
+//           chatLogs.push(data);
+//           this.setState({ chatLogs: chatLogs });
+//         },
+//         create: function(message) {
+//           this.perform("create", {
+//             content: message,
+//             user_id: user_id,
+//             user_name: user_name
+//           });
+//         }
+//       }
+//     );
+//   };
+//
+//   renderChatLog() {
+//     console.log('IN RENEDER CHAT', this.state)
+//     debugger;
+//     return this.state.chatLogs.map((el, i) => {
+//       if(el.user_name === null){
+//         return(
+//           <li key={`chat_${i}`}>
+//             <span className="chat-message">{"Anonymous"}</span>
+//             <span className="chat-message">{el.content}</span>
+//             <span className="chat-created_at">{moment(el.created_at).format(
+//               "h:mm:ss a"
+//             )}</span>
+//           </li>
+//         );
+//       }
+//       else{
+//         return (
+//           <li key={`chat_${i}`}>
+//             <span className="chat-message"><strong>{el.user_name}: </strong>{el.content}</span>
+//
+//             <span className="chat-created_at">{moment(el.created_at).format(
+//               "h:mm:ss a"
+//             )}</span>
+//           </li>
+//         );
+//       }
+//     });
+//   }
+//
+//   render() {
+//     if (this.state.loading) {
+//       return <h1>Initiating Chatroom...</h1>;
+//     }
+//
+//     return (
+//       <div className="App">
+//         <div className="stage">
+//           <h1>Chat</h1>
+//           <div className="chatBox">
+//             <ul className="chat-logs">{this.renderChatLog()}</ul>
+//           </div>
+//           <input
+//             onKeyPress={e => this.handleChatInputKeyPress(e)}
+//             value={this.state.currentChatMessage}
+//             onChange={e => this.updateCurrentChatMessage(e)}
+//             type="text"
+//             placeholder="Enter your message..."
+//             className="chat-input"
+//             autoFocus
+//           />
+//
+//           <RaisedButton
+//            className="send"
+//             label="Send"
+//             primary={true}
+//             onClick={e=> this.handleSendEvent(e)}
+//           />
+//
+//
+//         </div>
+//         < Footer />
+//       </div>
+//     );
+//   }
+//
+//   updateCurrentChatMessage(event) {
+//     this.setState({
+//       currentChatMessage: event.target.value
+//     });
+//   }
+//
+//   handleChatInputKeyPress(event) {
+//     if (event.key === "Enter") {
+//       this.handleSendEvent(event);
+//     } //end if
+//   }
+//
+//   handleSendEvent(event) {
+//     event.preventDefault();
+//     this.chats.create(this.state.currentChatMessage);
+//     this.setState({
+//       currentChatMessage: ""
+//     });
+//   }
+// }
+//
+// export default App;
+
+
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       currentChatMessage: "",
       chatLogs: [],
-      user: {},
+      user: null,
       loading: false
     };
   }
-  componentWillMount() {
-    debugger;
-    const user = jwtDecoder(localStorage.getItem('jwtToken'));
-    // const user = await jwtDecoder(this.props.token);
-    console.log('CHAT USER',user);
-     this.setState({
-      user:user,
-      loading: true
-    });
-    console.log(this.state.user);
-    this.fetchHistoryMessages();
-    this.createSocket();
 
-  }
   componentDidMount = async () => {
-    debugger
-    const user = jwtDecoder(localStorage.getItem('jwtToken'));
     // const user = await jwtDecoder(this.props.token);
-    console.log('CHAT USER',user);
+    const user = await jwtDecoder(this.props.token);
     await this.setState({
       user:user,
       loading: true
     });
 
-     this.fetchHistoryMessages();
+    await this.fetchHistoryMessages();
 
     await this.createSocket();
 
@@ -152,30 +313,26 @@ class App extends Component {
   };
 
   fetchHistoryMessages = () => {
-    axios(
-      {
-        url: 'https://codersappserver.herokuapp.com/messages.json'
 
-      }
-    )
+
+    axios.get(`https://codersappserver.herokuapp.com/messages.json`)
       .then(res => {
-        // console.log(chatLogs);
         this.setState({
           chatLogs: res.data
         })
       })
-      this.renderChatLog();
   }
 
   createSocket = () => {
-    // debugger;
     const user_name = this.state.user.name;
+    const concert = this.state.concert;
     const user_id = this.state.user.sub;
-    console.log(user_id, user_name);
+
     let cable = Cable.createConsumer("wss://codersappserver.herokuapp.com/cable");
     this.chats = cable.subscriptions.create(
       {
-        channel: `MessagesChannel`
+        channel: `MessagesChannel`,
+        event_id: this.state.concert.id
       },
       {
         connected: () => {},
@@ -196,8 +353,7 @@ class App extends Component {
   };
 
   renderChatLog() {
-    console.log('IN RENEDER CHAT', this.state)
-    debugger;
+    console.log(this.state)
     return this.state.chatLogs.map((el, i) => {
       if(el.user_name === null){
         return(
@@ -213,8 +369,8 @@ class App extends Component {
       else{
         return (
           <li key={`chat_${i}`}>
-            <span className="chat-message"><strong>{el.user_name}: </strong>{el.content}</span>
-
+            <span className="chat-message">{el.user_name}</span>
+            <span className="chat-message">{el.content}</span>
             <span className="chat-created_at">{moment(el.created_at).format(
               "h:mm:ss a"
             )}</span>
@@ -245,15 +401,9 @@ class App extends Component {
             className="chat-input"
             autoFocus
           />
-
-          <RaisedButton
-           className="send"
-            label="Send"
-            primary={true}
-            onClick={e=> this.handleSendEvent(e)}
-          />
-
-
+          <button onClick={e => this.handleSendEvent(e)} className="send">
+            Send
+          </button>
         </div>
         < Footer />
       </div>
